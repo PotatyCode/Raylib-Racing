3aec0db3a9140b262d3e11deda3b019d8c986e68
PotatyCode <Potatyyman@proton.me>
2025-10-28 18:26:51 +1000

    added friction and clamp

--- racing/include/carMove.hpp
+++ racing/include/carMove.hpp
@@ -8,6 +8,7 @@ class carMovement : component {
     const float ACCEL = 5.0f;
     const float DEACCEL = 10.0f;
     const float BASE_TURN = 2.5f;
+    const float FRICTION = 0.98f;
     Vector3 forward = {0, 0, 1};
     float speed = 0.0f;
 
@@ -29,18 +30,19 @@ class carMovement : component {
                 float turnSpeed = (BASE_TURN * (speed / MAX_SPEED)) * GetFrameTime();
                 Quaternion turnRotation = QuaternionFromAxisAngle(raylib::Vector3{0, 1, 0}, turnSpeed);
                 getParent().setRotation(QuaternionMultiply(getParent().getRotation(), turnRotation));
-                forward = Vector3RotateByQuaternion({0, 0, 1}, getParent().getRotation());
                 break;
             }
             case KEY_D: {
                 float turnSpeed = -BASE_TURN * (speed / MAX_SPEED) * GetFrameTime();
                 Quaternion turnRotation = QuaternionFromAxisAngle(raylib::Vector3{0, 1, 0}, turnSpeed);
                 getParent().setRotation(QuaternionMultiply(getParent().getRotation(), turnRotation));
-                forward = Vector3RotateByQuaternion({0, 0, 1}, getParent().getRotation());
                 break;
             }
         }
-        Vector3 velocity = Vector3Scale(Vector3Scale(forward, speed), GetFrameTime());
+        speed *= FRICTION;
+        speed = Clamp(speed, -MAX_SPEED * 0.5f, MAX_SPEED);
+        forward = Vector3RotateByQuaternion({0, 0, 1}, getParent().getRotation());
+        Vector3 velocity = Vector3Scale(forward, speed * GetFrameTime());
         getParent().setPosition(Vector3Add(velocity, getParent().getPosition()));
     }
 };
